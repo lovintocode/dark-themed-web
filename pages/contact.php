f862b2a30db81bf439b47bc0c17240fd238bd14b
Lovintocode <marcviagil@gmail.com>
2021-10-14 07:43:18 +0200

    Contact page creation

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,8 @@
 <div id="contact">
   <div class="show">
-    <img class="image" src="img/photos/slider-home1.jpg" alt="">
+    <div class="image-container">
+      <img class="image" src="img/photos/person1.jpg" alt="">
+    </div>
     <div class="text-container">
       <h2 class="title">Contact us</h2>
       <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -8,13 +10,30 @@
   </div>
   <div class="team">
     <div class="card">
-      <img class="image" src="" alt="">
-      <h3 class="name"></h3>
-      <p class="position"></p>
-      <p class="experience"></p>
+      <img class="image" src="img/photos/person1.jpg" alt="">
+      <h3 class="name">Tom Holland</h3>
+      <p class="position">Development Manager</p>
+      <p class="experience">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo exercitationem a odit</p>
+    </div>
+    <div class="card">
+      <img class="image" src="img/photos/person2.jpg" alt="">
+      <h3 class="name">Mark Martínez</h3>
+      <p class="position">Product Leader</p>
+      <p class="experience">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo exercitationem a odit</p>
+    </div>
+    <div class="card">
+      <img class="image" src="img/photos/person3.jpg" alt="">
+      <h3 class="name">Sarah Poliska</h3>
+      <p class="position">Programmer</p>
+      <p class="experience">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo exercitationem a odit</p>
     </div>
   </div>
-  <div class="form">
-    
+  <div class="form-container">
+    <h2 class="title">GET IN TOUCH</h2>
+    <form class="form" action="">
+      <input class="form-input" type="text" placeholder="Name">
+      <input class="form-input" type="text" placeholder="Email Adress">
+      <input class="btn" type="button" value="Send">
+    </form>
   </div>
 </div>
